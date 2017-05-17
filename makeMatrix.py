1eed7aac796656c994d974c10619479c14494c58
bweedop <bweedop@aggiemail.usu.edu>
2017-05-17 14:12:37 -0600

    May17

--- makeMatrix.py
+++ makeMatrix.py
@@ -1,26 +1,31 @@
 from Bio import SeqIO
 
+
 def getSeqid(file):
-    #This function records the names and the sequences within the nexus file.
-    # The names of the sequences are placed in the seqid list and will be used to create the first column of the
+    # This function records the names and the sequences within the nexus file.
+    # The names of the sequences are placed in the seq_id list and will be used to create the first column of the
     # matrix.
-    seqid = []
+    seq_id = []
     with open(file, 'r') as fullFile:
         for record in SeqIO.parse(fullFile, "nexus"):
-            seqid.append(record.id)
-    #Sequence names have been recorded in the list and are returned.
-    return seqid
+            seq_id.append(record.id)
+    # Sequence names have been recorded in the list and are returned.
+    return seq_id
+
+
 def getSeqs(file):
-    #The sequences will be recorded and used to find which of the seqs have the event.
+    # The sequences will be recorded and used to find which of the seqs have the event.
     seqs = []
     with open(file, 'r') as fullFile:
         for record in SeqIO.parse(fullFile, "nexus"):
             seqs.append(record.seq)
-    #Sequences have been recorded in the list and are returned.
+    # Sequences have been recorded in the list and are returned.
     return seqs
+
+
 def getEventNames(file):
-    #The gene and the event names will be recorded in the allEvents list.
-    allEvents = []
+    # The gene and the event names will be recorded in the all_events list.
+    all_events = []
     with open(file, 'r') as handle:
         handle = handle.read()
         for lines in handle.splitlines():
@@ -33,29 +38,30 @@ def getEventNames(file):
                             if event == '':
                                 pass
                             elif event != '':
-                                #All the names of genes and events are recorded.
-                                allEvents.append(event)
+                                # All the names of genes and events are recorded.
+                                all_events.append(event)
+    return all_events
 
-    return allEvents
 
-def getLocations(file, events):
-    #Function takes the nexus file and the list of event names as arguments. The locations will be recorded in the
-    #  eventLocation list.
-    allLocations = []
+def getLocations(file, event):
+    # Function takes the nexus file and the event name as arguments. The locations will be recorded in the
+    #  all_Location list.
+    all_locations = []
     with open(file, 'r') as handle:
         handle = handle.read()
         for lines in handle.splitlines():
-            if 'CharSet'in lines:
-                eventsPlusLocations = lines.split('CharSet ')
-                currentEvent = events.pop(0)
-                for i in eventsPlusLocations:
-                    if currentEvent in i:
-                        locations = i.split(currentEvent+' = ')
+            if 'CharSet' in lines:
+                events_plus_locations = lines.split('CharSet ')
+                for i in events_plus_locations:
+                    if event in i:
+                        locations = i.split(event + ' = ')
                         for j in locations:
-                            eventLocation = j.split(' ')
-                            for k in eventLocation:
-                                firstPositions = k.split()
-                                for l in firstPositions:
-                                    l = l.replace(';','')
-                                    allLocations.append(l)
-    print(allLocations)
+                            event_location = j.split(' ')
+                            for k in event_location:
+                                first_positions = k.split()
+                                for l in first_positions:
+                                    l = l.replace(';', '')
+                                    all_locations.append(l)
+    return all_locations
+
+
